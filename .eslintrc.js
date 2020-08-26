c2c5007172a8f1047d523800513534496f246d05
linyongxin <https://github.com/a1102072495>
2020-08-26 16:52:32 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+
+  extends: ['plugin:vue/essential', '@vue/standard'],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'space-before-function-paren': 0,
+    'eol-last': 0,
+    'no-trailing-spaces': 0
+  },
+
+  extends: ['plugin:vue/essential', '@vue/standard']
 }
---
+++ src/components/Login.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像区域 -->
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <!-- ref 获取表单的引用对象 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login-form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="el-icon-lock" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginFrom">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 16,
+            message: '长度在 6 到 16 个字符',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置表单
+    resetLoginFrom() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 登录函数
+    login() {
+      // 登录预验证 validate() 对整个表单进行校验的方法 参数为一个回调函数。该回调函数会在校验结束后被调用，并传入两个参数：是否校验成功和未通过校验的字段。
+      this.$refs.loginFormRef.validate(async val => {
+        if (!val) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status === 400) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 1.将登录成功后的 token 保存到客户端的 sessionStroage 中 
+        //  1.1 因为项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2.通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar-box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    overflow: hidden;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    box-shadow: 0 0 10px #eee;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #edeeec;
+    }
+  }
+}
+.login-form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
